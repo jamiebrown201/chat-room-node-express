3b18cace8cbc6a48802e06627db08e33a60627a4
Jamie Brown <jamiebrown201@hotmail.com>
2016-04-06 17:22:12 +0100

    basic styling

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 
 
-app.get('/' , function(req, res) {
-
+app.get('/hello' , function(req, res) {
+  res.send('hello worlds!');
 });
 
 app.listen(3000, function() {
-  console.log('Chat app is listening on port 3000')
+  console.log('Chat app is listening on port 3000');
 });
