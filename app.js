aeb6673de1810f64e32ae2a38564a25b41f41e8c
Jamie Brown <jamiebrown201@hotmail.com>
2016-04-06 21:05:17 +0100

    add jade support and basic views

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+
+app.set("views", "./views");
+app.set('view engine', 'jade');
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 
+app.get('/' , function(req, res) {
+  res.render("index", { title: "Home"});
+});
 
-app.get('/hello' , function(req, res) {
-  res.send('hello worlds!');
+app.get('/admin/rooms', function (req, res) {
+    res.render("rooms", { title: "Rooms"});
 });
 
 app.listen(3000, function() {
