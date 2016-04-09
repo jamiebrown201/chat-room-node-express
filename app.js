f508052aa30dffd21ce1ff5c244b4ff006436ac1
Jamie Brown <jamiebrown201@hotmail.com>
2016-04-09 13:39:30 +0100

    passport autherntication

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express = require("express");
 var app = express();
 var rooms = require("./data/rooms.json");
 var bodyParser = require("body-parser");
+var passport = require("passport");
+require("./passport-init");
 
 app.set("views", "./views");
 app.set('view engine', 'jade');
-
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
@@ -16,6 +17,24 @@ require('express-debug')(app, {});
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
+app.use(require('express-session')({
+  secret: 'keyboard cat', resave: false, saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+var authRouter = require('./auth');
+app.use(authRouter);
+
+app.use(function(req,res,next){
+  if(req.isAuthenticated()) {
+    res.locals.user = req.user;
+    next();
+    return;
+  }
+  res.redirect("/login");
+});
 
 app.get('/' , function(req, res) {
   res.render("home", { title: "Home"});
@@ -26,6 +45,7 @@ app.use("/admin", adminRouter);
 
 var apiRouter = require('./api');
 app.use("/api", apiRouter);
+
 
 app.listen(3000, function() {
   console.log('Chat app is listening on port 3000');
